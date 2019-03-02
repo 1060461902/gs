45ec29ce981fcf68629309a054c26a7e572333ec
name <s>
2019-03-02 12:36:00 +0800

    123

--- pages/ask/ask.js
+++ pages/ask/ask.js
@@ -1,17 +1,60 @@
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    userInfo: {},
+    message_list:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  onLoad:function(options){
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo
+          })
+        }
+      })
+    }
+
+    let message_list = this.data.message_list;
+    message_list.push({
+      type: "reply-item-text",
+      data: { text: "您好，小智为您服务" }
+    });
+    this.setData({
+      message_list
+    });
   },
 
+  sendMsg:function(e){
+    let that= this;
+    let msg = e.detail.value;
+    let message_list = this.data.message_list;
+    message_list.push({
+      type: "ask-item",
+      data: { text: msg, head_url: that.data.userInfo.avatarUrl }
+    });
+    this.setData({
+      message_list
+    });
+  }
 })
