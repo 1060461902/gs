36991644dc7dd57023eca983d86e52f4d08bca4c
name <s>
2019-03-05 15:55:08 +0800

    反馈item完成

--- pages/ask/ask.js
+++ pages/ask/ask.js
@@ -12,6 +12,7 @@ Page({
   },
 
   onLoad:function(options){
+    //获取userInfo
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo
@@ -36,16 +37,25 @@ Page({
       })
     }
 
+    //默认欢迎
     let message_list = this.data.message_list;
     message_list.push({
       type: "reply-item-text",
       data: { text: "您好，小智为您服务" }
+    });
+    message_list.push({
+      type:'reply-item-feedback',
+      data:{
+        position: message_list.length,
+        id:1
+      }
     });
     this.setData({
       message_list
     });
   },
 
+  //底部输入框点击发送时调用
   sendMsg:function(e){
     let that= this;
     let msg = e.detail.value;
@@ -60,9 +70,99 @@ Page({
     });
   },
 
+  //底部输入框变化时改变message的值
   bindMessage:function(e){
     this.setData({
       message:e.detail.value
+    });
+  },
+
+  //点击反馈的满意按钮
+  content:function(e){
+    let position = e.target.dataset.position;
+    let message_list = this.data.message_list;
+    message_list.splice(position, 1, {
+      type: "reply-item-text",
+      data: { text: "感谢您的反馈！如还有问题，您可继续提问。" }
+    });
+    this.setData({
+      message_list
+    });
+  },
+
+  //点击反馈的不满意按钮
+  discontent:function(e){
+    let position = e.target.dataset.position;
+    let message_list = this.data.message_list;
+    message_list.splice(position, 1, {
+      type: "reply-item-discontent",
+      data: {
+        id:1,
+        position
+      }
+    });
+    this.setData({
+      message_list
+    });
+  },
+
+  //选择不满意的原因
+  selectDiscontentReason:function(e){
+    let selected = e.detail.value;
+    let position = e.target.dataset.position;
+    let message_list = this.data.message_list;
+    let data = message_list[position].data;
+    data.selected = selected;
+    data.show_button = true;
+    if(selected == 2){
+      data.show_input = true;
+    }else{
+      data.show_input = false;
+    }
+    message_list[position].data = data;
+    this.setData({
+      message_list
+    });
+  },
+
+  //输入不满意的原因
+  discontentDetailInput:function(e){
+    let input = e.detail.value;
+    let position = e.target.dataset.position;
+    let message_list = this.data.message_list;
+    let data = message_list[position].data;
+    data.detail = input;
+    message_list[position].data = data;
+    this.setData({
+      message_list
+    });
+  },
+
+  //发送不满意的原因
+  sendDiscontentReason:function(e){
+    let message_list = this.data.message_list;
+    let position = e.target.dataset.position;
+    let data = message_list[position].data;
+    console.log(data);
+    message_list.splice(position,1,{
+      type: "reply-item-text",
+      data: { text: "感谢您的反馈！如还有问题，您可继续提问。" }
+    });
+    this.setData({
+      message_list
+    });
+  },
+
+  //取消发送反馈
+  cancelDiscontentReason:function(e){
+    let message_list = this.data.message_list;
+    let position = e.target.dataset.position;
+    message_list.splice(position, 1, {
+      type: "reply-item-text",
+      data: { text: "已取消反馈，如还有问题，您可继续提问。" }
+    });
+    this.setData({
+      message_list
     });
   }
 })
