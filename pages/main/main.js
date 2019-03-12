af237cd1edf4646c17ff2dabe79f18625efc05ae
name <s>
2019-03-12 11:43:07 +0800

    123

--- pages/main/main.js
+++ pages/main/main.js
@@ -6,13 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo:{}
+    userInfo:{},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -39,6 +41,15 @@ Page({
         }
       })
     }
+  },
+  
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   toAskPage:function(){
